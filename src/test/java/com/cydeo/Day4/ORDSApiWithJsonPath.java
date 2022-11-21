f9dfd5878621f4f28bbdb2c5fe4f84a1ad2428c7
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-11-21 16:25:55 +0100

    day4

---
+++ src/test/java/com/cydeo/Day4/ORDSApiWithJsonPath.java
@@ -0,0 +1,11 @@
+package com.cydeo.Day4;
+
+import com.cydeo.utilities.HRTestBase;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ORDSApiWithJsonPath extends HRTestBase {
+
+
+}
