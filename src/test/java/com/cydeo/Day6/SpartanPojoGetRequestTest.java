5d233ce00568faf5f0da0523f6c3abf61641166c
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-11-27 18:11:57 +0100

    day6

--- src/test/java/com/cydeo/Day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cydeo/Day6/SpartanPojoGetRequestTest.java
@@ -1,7 +1,9 @@
 package com.cydeo.Day6;
 
+import com.cydeo.pojo.Search;
 import com.cydeo.pojo.Spartan;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,7 +35,54 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         System.out.println("spartan15.getId() = " + spartan15.getId());
         System.out.println("spartan15.getGender() = " + spartan15.getGender());
 
+
+        //second way of deserialize json to java
+        //2.using JsonPath to deserialize to custom class
+        JsonPath jsonPath = response.jsonPath();
+        Spartan s15 = jsonPath.getObject("",Spartan.class);
+        System.out.println("s15.getName() = " + s15.getName());
+        System.out.println("s15.getPhone() = " + s15.getPhone());
+
     }
 
+    @DisplayName("Get one spartan from search endpoint result and use POJO")
+    @Test
+    public void spartanSearchWithPojo(){
+
+        // /spartans/search?nameContains=a&gender=Male
+        // send get request to above endpoint and save first object with type Spartan POJO
+
+        JsonPath jsonPath = given().accept(ContentType.JSON).and().queryParams("nameContains"
+                ,"a","gender","Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().jsonPath();
+
+        //get the first spartan from content list and put inside spartan object
+
+        Spartan s1 = jsonPath.getObject("content[0]",Spartan.class);
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s1.getName() = " + s1.getName());
+        System.out.println("s1.getGender() = " + s1.getGender());
+
+
+    }
+
+    @Test
+    public void test3(){
+
+        Response response = given().accept(ContentType.JSON).and().queryParams("nameContains"
+                ,"a","gender","Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().response();
+
+        Search searchResult = response.as(Search.class);
+
+        System.out.println(searchResult.getContent().get(0).getName());
+
+
+    }
 
 }
