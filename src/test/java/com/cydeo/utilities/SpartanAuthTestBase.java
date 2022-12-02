e20cfb62393a299792b022ea5a135d6da64b20c0
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-12-02 10:14:01 +0100

    day8 Auth

---
+++ src/test/java/com/cydeo/utilities/SpartanAuthTestBase.java
@@ -0,0 +1,22 @@
+package com.cydeo.utilities;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class SpartanAuthTestBase {
+
+    //BeforeAll is an annotation equals to @BeforeClass in testNg, we use with static method name
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+
+        baseURI = "http://3.86.103.110:7000";
+
+    }
+
+
+
+
+}
