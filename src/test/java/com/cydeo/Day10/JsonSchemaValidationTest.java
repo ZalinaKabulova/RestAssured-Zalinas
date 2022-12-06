14bba9b3c128bed4da6b0d934e86af18c4ddb89f
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-12-06 12:57:56 +0100

    day10 Valid certificate

--- src/test/java/com/cydeo/Day10/JsonSchemaValidationTest.java
+++ src/test/java/com/cydeo/Day10/JsonSchemaValidationTest.java
@@ -6,6 +6,8 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -28,6 +30,22 @@ public class JsonSchemaValidationTest extends SpartanAuthTestBase {
                 .statusCode(200)
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"))
                 .log().all();
+
+    }
+
+    @DisplayName("GET request to all spartans and verify schema")
+    @Test
+    public void allSpartanSchemaTest(){
+
+        given()
+                .accept(ContentType.JSON)
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                //what if you have your .json file not under resources following way -->
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cydeo/Day10/spartanPostJsonSchema.json")));
 
     }
 
